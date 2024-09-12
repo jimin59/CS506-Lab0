2b0e6776557ac2a3717dd9f5a9c629bca49819ad
박지민 <jimin@crc-dot1x-nat-10-239-111-65.bu.edu>
2024-09-12 16:00:55 -0400

    Implement functions in utils.py and add tests

--- utils.py
+++ utils.py
@@ -1,15 +1,13 @@
-## Please fill in all the parts labeled as ### YOUR CODE HERE
-
 import numpy as np
 
 def dot_product(v1, v2):
     '''
     v1 and v2 are vectors of same shape.
-    return the scalar dor product of the two vectors.
+    return the scalar dot product of the two vectors.
     # Hint: use `np.dot`.
     '''
-    ### YOUR CODE HERE
-    
+    return np.dot(v1, v2)
+
 def cosine_similarity(v1, v2):
     '''
     v1 and v2 are vectors of same shape.
@@ -26,8 +24,11 @@ def cosine_similarity(v1, v2):
     
     # Hint: Use `dot_product` and `np.linalg.norm`.
     '''
-    ### YOUR CODE HERE
-    
+    dot_prod = dot_product(v1, v2)
+    norm1 = np.linalg.norm(v1)
+    norm2 = np.linalg.norm(v2)
+    return dot_prod / (norm1 * norm2)
+
 def nearest_neighbor(target_vector, vectors):
     '''
     target_vector is a vector of shape d.
@@ -38,4 +39,14 @@ def nearest_neighbor(target_vector, vectors):
     # Hint: You should use the cosine_similarity function that you already wrote.
     # Hint: For this lab, you can just use a for loop to iterate through vectors.
     '''
-    ### YOUR CODE HERE
+    best_index = -1
+    best_similarity = -1
+    
+    for i, vector in enumerate(vectors):
+        similarity = cosine_similarity(target_vector, vector)
+        if similarity > best_similarity:
+            best_similarity = similarity
+            best_index = i
+    
+    return best_index
+
